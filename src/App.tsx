ffa742ac76224867b7883affe7c11da625b4c098
dnhmnn <daniel@heilmann.bayern>
2026-03-21 08:38:39 +0100

    Remove user prop from route components
    Removed user prop from SettingsPage and AusbildungenPage routes.

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,17 @@ import Login from './pages/Login'
 import Index from './pages/Index'
 import SettingsPage from './pages/SettingsPage'
 import AusbildungenPage from './pages/AusbildungenPage'
-import { useAuth } from './hooks/useAuth'
 import './styles/globals.css'
 
 function App() {
-  const { user } = useAuth()
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/hub" element={<Hub />} />
-        <Route path="/settings" element={<SettingsPage user={user} />} />
-        <Route path="/ausbildungen" element={<AusbildungenPage user={user} />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/ausbildungen" element={<AusbildungenPage />} />
       </Routes>
     </BrowserRouter>
   )
