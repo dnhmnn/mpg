e405de5709ca894e76085508dfbbf080ba0eb8c6
dnhmnn <daniel@heilmann.bayern>
2026-03-22 12:53:09 +0100

    Remove user name and logout options from StatusBar
    Removed user name display and logout options from StatusBar.

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -44,18 +44,3 @@ export default function StatusBar({ user, onLogout, showBackButton, onBackClick,
     </div>
   )
 }
-        </div>
-      )}
-      <div className="user-name">{pageName || userName}</div>
-      {showHubLink ? (
-        <Link to="/hub" className="logout-btn">
-          Hub
-        </Link>
-      ) : (
-        <button className="logout-btn" onClick={onLogout}>
-          Abmelden
-        </button>
-      )}
-    </div>
-  )
-}
