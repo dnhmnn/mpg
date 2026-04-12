e9a49703bddef87af2718718aee8dcac9ca09f0d
dnhmnn <daniel@heilmann.bayern>
2026-04-12 15:01:48 +0200

    Update useAusbildungenData.ts

--- src/pages/ausbildungen/hooks/useAusbildungenData.ts
+++ src/pages/ausbildungen/hooks/useAusbildungenData.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import PocketBase from 'pocketbase'
+import { pb } from '../../../lib/pocketbase'
 import { Termin, Teilnehmer, TerminTeilnehmer, Dokument, Modul, ModulTermin, ModulProgress } from '../types/ausbildungen.types'
 
 const pb = new PocketBase('https://api.responda.systems')
