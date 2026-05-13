0be6b4430c8b0a39be71e9f340f8adfe9128b3de
dnhmnn <daniel@heilmann.bayern>
2026-05-13 23:08:59 +0200

    OrgProduktausgabe: inventory_items ohne organization_id Filter laden

--- src/pages/public/OrgProduktausgabe.tsx
+++ src/pages/public/OrgProduktausgabe.tsx
@@ -211,10 +211,9 @@ export default function OrgProduktausgabe() {
 
   useEffect(() => {
     pb.collection('inventory_items').getFullList<InventoryItem>({
-      filter: `organization_id = "${org.id}"`,
       sort: 'name',
     }).then(setInventoryItems).catch(() => {})
-  }, [org.id])
+  }, [])
 
   const addPos = () => setPositions(p => [...p, { qty: 1 }])
   const delPos = (i: number) => setPositions(p => p.filter((_, j) => j !== i))
